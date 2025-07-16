6926dc0932be88717b8d7df624f45c975df4df8d
Sam <tongyuhui4545@gmail.com>
2025-07-16 21:48:28 +1000

    feat: middleware

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -34,7 +34,7 @@ class ListingController extends Controller
         //
         Listing::create($request->all());
 
-        return redirect()->route('listing.index');
+        return redirect()->route('listing.index')->with('success', 'Listing created successfully');
     }
 
     /**
