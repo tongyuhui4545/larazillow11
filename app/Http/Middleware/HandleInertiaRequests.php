a707b24029ae0e21db0622bbda62ab6ae7e4a939
Sam <tongyuhui4545@gmail.com>
2025-07-17 22:05:19 +1000

    feat: middleware flash

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,9 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             //
+            'flash' => [
+                'success' => $request->session()->get('success'),
+            ]
         ];
     }
 }
